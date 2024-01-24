16d612bf1ec24a6c7c18b773e1520bbb0451e5c9
Ritikvishwakarma62 <rv4559751@gmail.com>
2024-01-24 10:36:36 -0800

    ritik

--- project/src/modules/AdminProtectedModule.js
+++ project/src/modules/AdminProtectedModule.js
@@ -1,17 +1,16 @@
 import React,{useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
 const AdminProtectedModule = () => {
-
     let navigate = useNavigate()
     useEffect(()=>{
         if(! localStorage.getItem("admin-token")){
             navigate("/")
         }
     },[])
-
   return (
-    <div>AdminProtectedModule</div>
+    <Outlet/>
   )
 }
 
